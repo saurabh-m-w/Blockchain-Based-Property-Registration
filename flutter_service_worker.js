c24262cb550ee23f102acc90ed1524c6b6695724
saurabh-m-w <smwaghmare2016@gmail.com>
2022-01-25 16:56:48 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/splash.png": "cd84b6a309528f895ee54959c3c58b71",
-"index.html": "38dc7d7fd582ef0d501163fece293256",
-"/": "38dc7d7fd582ef0d501163fece293256",
-"main.dart.js": "028c60aeaba3abc57b79438868c517a5",
+"index.html": "5b456244e23c5f6b0f5fdf12e372f35c",
+"/": "5b456244e23c5f6b0f5fdf12e372f35c",
+"main.dart.js": "462fa4a9b348d57ae69121141240cf45",
 "manifest.json": "64c400d89aff170ae720625c32dde379",
 "version.json": "f895f70c1fadcf3c216bbaf797d24fea"
 };
